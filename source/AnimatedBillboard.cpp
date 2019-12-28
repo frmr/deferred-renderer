cf16207bea7b7d0dd9c7c18e74bae630c20ef796
Tom Farmer <tfarmer@hamamatsu.co.uk>
2019-12-28 19:07:07 +0000

    Removed superfluous files

--- source/AnimatedBillboard.cpp
+++
@@ -1,9 +0,0 @@
-#include "AnimatedBillboard.h"
-
-AnimatedBillboard::AnimatedBillboard( const BillboardType type, map<string, vector<Frame>>* const animations )
-	: type( type ),
-      frameTimer( 0 ),
-	  frameIndex( 0 ),
-	  animations( animations )
-{
-}
